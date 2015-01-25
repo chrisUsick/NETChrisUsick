98aac2d900b547dbd27227ba435f0a71c76956e0
chrisUsick <christopher.usick@gmail.com>
2015-01-24 20:08:05 -0600

    added automotive manager class

---
+++ NETChrisUsick/AutomotiveManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NETChrisUsick
+{
+    public class AutomotiveManager
+    {
+        public enum DialogResult
+        {
+            OK
+        }
+        private static bool isBeingTested = false;
+        
+        //private static DialogResult dialogResult = DialogResult.OK;
+        
+        public static bool IsBeingTested {get;}
+        //public static DialogResult DialogResult {get;} 
+
+        public static bool IsNumeric(string stringValue)
+        {
+            double maybeDouble;
+            return Double.TryParse(stringValue, out maybeDouble);
+        }
+    }
+}
