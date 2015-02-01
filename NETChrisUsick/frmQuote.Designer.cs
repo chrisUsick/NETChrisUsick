27efdd9e62004add0c70ca74522a263d507b0295
chrisUsick <christopher.usick@gmail.com>
2015-02-01 16:45:47 -0600

    started frmQuote

--- NETChrisUsick/frmQuote.Designer.cs
+++ NETChrisUsick/frmQuote.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalePrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTradeIn = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vehicle\'s S&ale Price:";
+            // 
+            // txtSalePrice
+            // 
+            this.txtSalePrice.Location = new System.Drawing.Point(132, 20);
+            this.txtSalePrice.Name = "txtSalePrice";
+            this.txtSalePrice.Size = new System.Drawing.Size(114, 20);
+            this.txtSalePrice.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "T&rade-in Allowance:";
+            // 
+            // txtTradeIn
+            // 
+            this.txtTradeIn.Location = new System.Drawing.Point(133, 47);
+            this.txtTradeIn.Name = "txtTradeIn";
+            this.txtTradeIn.Size = new System.Drawing.Size(113, 20);
+            this.txtTradeIn.TabIndex = 3;
             // 
             // frmQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(636, 460);
+            this.Controls.Add(this.txtTradeIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSalePrice);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmQuote";
             this.Text = "Sales Quote";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSalePrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTradeIn;
 
     }
 }
