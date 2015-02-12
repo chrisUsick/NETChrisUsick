6f3e49adadd5513debabe3a83f84ceaa5b896a63
chrisUsick <christopher.usick@gmail.com>
2015-02-12 15:57:56 -0600

    asgn 4 started;
    Invoice and serviceInvoice created

---
+++ BusinessTier/Invoice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessTier
+{
+    abstract class Invoice
+    {
+        /// <summary>
+        /// read only property to get the PST applied to this sale
+        /// </summary>
+        public double PSTRate { get; private set; }
+
+        /// <summary>
+        /// read-only property to get the GST applied to this sale
+        /// </summary>
+        public double GSTRate { get; private set; }
+
+        /// <summary>
+        /// read only property storing the amount of PST charged
+        /// </summary>
+        public abstract double PSTCharged
+        {
+            get;
+        }
+
+        /// <summary>
+        /// read only property storing the amount of GST charged
+        /// </summary>
+        public abstract double GSTCharged
+        {
+            get;
+        }
+
+        /// <summary>
+        /// read only property storing the subTotal of this invoice
+        /// </summary>
+        public abstract double SubTotal
+        {
+            get;
+        }
+
+        /// <summary>
+        /// read only property to store the total cost of this invoice
+        /// </summary>
+        public abstract double Total
+        {
+            get;
+        }
+
+        /// <summary>
+        /// constructs an invoice, sets the tax rates
+        /// </summary>
+        /// <param name="pstRate">PST to apply to the invoice</param>
+        /// <param name="gstRate">GST to apply to the invoice</param>
+        public Invoice(double pstRate, double gstRate)
+        {
+            //set this objects PST and GST rate
+            this.PSTRate = pstRate;
+            this.GSTRate = gstRate;
+        }       
+    }
+}
---
+++ BusinessTier/ServiceInvoice.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessTier
+{
+    public class ServiceInvoice : Invoice
+    {
+        /// <summary>
+        /// the cost of labour for this invoice
+        /// </summary>
+        public double LabourCost { get; set; }
+
+        /// <summary>
+        /// the cost of parts for this invoice
+        /// </summary>
+        public double PartsCost { get; set; }
+
+        /// <summary>
+        /// the cost of materials for this invoice
+        /// </summary>
+        public double MaterialCost { get; set; }
+
+        /// <summary>
+        /// read only propertythe PST charged on this invoice
+        /// </summary>
+        public override double PSTCharged
+        {
+            get 
+            {
+                return SubTotal * PSTRate;
+            }
+        }
+
+        /// <summary>
+        /// read only property the GST charged on this invoice
+        /// </summary>
+        public override double GSTCharged
+        {
+            get 
+            {
+                return SubTotal * GSTRate;
+            }
+        }
+
+        /// <summary>
+        /// read only property sub total of the invoice
+        /// </summary>
+        public override double SubTotal
+        {
+            get 
+            {
+                // return the sum of all costs
+                return LabourCost + MaterialCost + PartsCost;
+            }
+        }
+
+        /// <summary>
+        /// read only property to return the total cost (subtotal plus taxes)
+        /// </summary>
+        public override double Total
+        {
+            get 
+            {
+                // return the subtotal and tax
+                return SubTotal + PSTCharged + GSTCharged;
+            }
+        }
+
+        /// <summary>
+        /// constructs A service invoice
+        /// </summary>
+        /// <param name="pstRate">the PST to apply</param>
+        /// <param name="gstRate">the GST to apply</param>
+        public ServiceInvoice(double pstRate, double gstRate)
+            : base(pstRate, gstRate) { }
+    }
+}
